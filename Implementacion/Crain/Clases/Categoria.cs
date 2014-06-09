866354ef5c3e6431877c76d4100298c097d59b44
carrizofm2010 <carrizofm2010@gmail.com>
2014-06-09 22:53:29 +0000

    proyecto crain

---
+++ Implementacion/Crain/Clases/Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases
+{
+    public class Categoria
+    {
+        private int idCategoria;
+        private int nombreCategoria;
+
+        public int IdCategoria
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int NombreCategoria
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
