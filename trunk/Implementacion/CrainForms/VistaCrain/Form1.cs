6b2dc4d3e1e02a438c7972947bf490ad83ea9331
walterjesusvale@gmail.com <walterjesusvale@gmail.com@f5c34dc9-2c78-0fd0-484a-ed71414e76ba>
2014-06-11 15:13:23 +0000

    Agregado de la carpeta MappingFiles y comenzando con hibernate

--- trunk/Implementacion/CrainForms/VistaCrain/Form1.cs
+++ trunk/Implementacion/CrainForms/VistaCrain/Form1.cs
@@ -6,14 +6,32 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace VistaCrain
 {
     public partial class Form1 : Form
     {
+       MySqlConnection conexion=new MySqlConnection();
+   
         public Form1()
         {
             InitializeComponent();
+            this.iniciarConexion();
+        }
+        private void iniciarConexion() {
+            try {
+                string servidor = "localhost";
+                string baseDatos = "dbms_crain";
+                string usuario = "root";
+                string password = "crain";
+                string cadenaConexion = "SERVER=" + servidor + ";" + "DATABASE=" + baseDatos + ";" + "UID=" + usuario + ";" + "PASSWORD=" + password + ";";  
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                MessageBox.Show("La conexion se  ha realizado con exito","Bien hecho!");
+            }catch(MySqlException){
+                MessageBox.Show("Ocurrio un error al intentar conectarse", "ERROR");
+            }
         }
     }
 }
