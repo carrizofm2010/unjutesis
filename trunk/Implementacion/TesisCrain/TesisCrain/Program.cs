e3adb7d4d03c95cf358b0d3469f3fc548d51c87e
walterjesusvale@gmail.com <walterjesusvale@gmail.com@f5c34dc9-2c78-0fd0-484a-ed71414e76ba>
2014-06-15 02:54:57 +0000

    Version funcional con NHibernate

---
+++ trunk/Implementacion/TesisCrain/TesisCrain/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TesisCrain.Dominio;
+using TesisCrain.DaoImpl;
+
+namespace TesisCrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  PruebaConexion prueba = new PruebaConexion();
+            Categoria categoria = new Categoria(4, "puto");
+            CategoriaDaoImpl daoImpl = new CategoriaDaoImpl();
+            daoImpl.saveCategoria(categoria);
+        }
+    }
+}
