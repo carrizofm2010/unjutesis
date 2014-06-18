a2c90f1fa6177e5ebe25e3072a922502aa6be5dd
walterjesusvale@gmail.com <walterjesusvale@gmail.com@f5c34dc9-2c78-0fd0-484a-ed71414e76ba>
2014-06-18 05:01:08 +0000

    NUEVA VERSION FUNCIONAL DEL PROYECTO-AGREGADO DE MAPEO DE VIDEO Y LOS CORRESPONDIENTES DAOIMPL

---
+++ trunk/Implementacion/TesisCrain/TesisCrain/DaoImpl/VideoDaoImpl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TesisCrain.Dominio;
+using TesisCrain.Dao;
+using TesisCrain.Config;
+using NHibernate;
+
+namespace TesisCrain.DaoImpl
+{
+    public class VideoDaoImpl:VideoDao
+    {
+        public void altaVideo(Video video) {
+            NHibernateManager manager = new NHibernateManager();
+            {
+
+                using (ITransaction transaction = manager.Session.BeginTransaction())
+                {
+                    manager.Session.Save(video);
+                    transaction.Commit();
+                }
+            }
+        }
+        public void modificarVideo(Video video) {
+            NHibernateManager manager = new NHibernateManager();
+            {
+
+                using (ITransaction transaction = manager.Session.BeginTransaction())
+                {
+                    manager.Session.Update(video);
+                    transaction.Commit();
+                }
+            }
+        }
+        public void bajaVideo(Video video) {
+            NHibernateManager manager = new NHibernateManager();
+            {
+
+                using (ITransaction transaction = manager.Session.BeginTransaction())
+                {
+                    manager.Session.Delete(video);
+                    transaction.Commit();
+                }
+            }
+        }
+
+    }
+}
---
+++ trunk/Implementacion/TesisCrain/TesisCrain/Dominio/Categoria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TesisCrain.Dominio
+{
+    public class Categoria
+    {
+        private int idCategoria;
+        private string nombreCategoria;
+        public Categoria() { }
+        public Categoria(int idCategoria, string nombreCategoria)
+        {
+            this.idCategoria = idCategoria;
+            this.nombreCategoria = nombreCategoria;
+        }
+
+        public virtual int IdCategoria
+        {
+            get
+            {
+                return idCategoria;
+            }
+            set
+            {
+                idCategoria = value;
+            }
+        }
+
+        public virtual string NombreCategoria
+        {
+            get
+            {
+                return nombreCategoria;
+            }
+            set
+            {
+                nombreCategoria = value;
+            }
+        }
+    }
+}
