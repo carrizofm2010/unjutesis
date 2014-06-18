fb4e3e90265e9bbc3693481696f68f25cceaef0a
walterjesusvale <walterjesusvale@gmail.com>
2014-06-18 19:05:10 +0000

    Servicios facade

---
+++ Implementacion/TesisCrain/TesisCrain/Servicios/Facade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TesisCrain.DaoImpl;
+using TesisCrain.Dominio;
+
+namespace TesisCrain.Servicios
+{
+    class Facade
+    {
+        VideoDaoImpl videoDaoImpl = new VideoDaoImpl();
+        DiarioDaoImpl diarioDaoImpl = new DiarioDaoImpl();
+        CategoriaDaoImpl categoriaDaoImpl = new CategoriaDaoImpl();
+        AdministradorDaoImpl ddministradorDaoImpl = new AdministradorDaoImpl();
+        
+        //Servicios de video
+        public void altaVideo(Video video) {
+            videoDaoImpl.altaVideo(video);
+        }
+        public void modificarVideo(Video video)
+        {
+            videoDaoImpl.modificarVideo(video);
+        }
+        public void bajaVideo(Video video)
+        {
+            videoDaoImpl.bajaVideo(video);
+        }
+        //Servicios de diario
+        //Servicios de categoria
+        //Servicios de administrador
+        
+    }
+}
